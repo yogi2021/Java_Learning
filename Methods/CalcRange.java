3901e86b18babdd427c176bc2621e31db1f8168d
yogi2021 <yp628256@gmail.com>
2026-07-24 17:31:34 +0530

    Another method practice: Calculate range of number

---
+++ Methods/CalcRange.java
@@ -0,0 +1,27 @@
+package Methods;
+import java.util.Scanner;
+public class CalcRange{
+
+	void displayOddrange(int start, int end) {
+		for (int i= start; i<=end; i++) {
+			if(i % 2 !=0) {
+				System.out.println(i);
+			}
+		}
+	}
+	
+public static void main(String[] args) {
+	CalcRange obj = new CalcRange();
+//	obj.displayEvenRange(12, 22);
+	
+	Scanner sc= new Scanner(System.in);
+	
+	System.out.println("Enetr starting number:");
+	int start = sc.nextInt();
+	
+	System.out.println("Enetr Ending number:");
+	int end = sc.nextInt();
+	obj.displayOddrange(start, end);
+	sc.close();
+}
+}
