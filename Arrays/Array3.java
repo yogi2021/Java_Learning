e6e8b38ded2e748a2080e0151eacc5aeb9726dff
yogi2021 <yp628256@gmail.com>
2026-08-07 17:26:03 +0530

    Adding sum changes in code

--- Arrays/Array3.java
+++ Arrays/Array3.java
@@ -7,7 +7,7 @@ public class Array3 {
 	void get(String arr[]) {
 		Scanner sc = new Scanner(System.in);
 		for(int i=0; i<arr.length; i++) {
-			System.out.println("Enter a Charecter");
+			System.out.println("Enter a Charecter : ");
 			arr[i]= sc.next();
 		}
 	}
