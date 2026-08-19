00bf0fb6f5e46e9bbc409a21682ebf1611318f3d
yogi2021 <yp628256@gmail.com>
2026-08-19 12:30:58 +0530

    String: methods and operation

---
+++ Strings/StringMethods2.java
@@ -0,0 +1,52 @@
+package Strings;
+
+public class StringMethods2 {
+
+    public static void main(String[] args) {
+
+        // String Creation
+        String str = "Python";
+
+        // length() -> Returns total number of characters
+        int length = str.length();
+        System.out.println("Length : " + length);
+
+        // Concat() -> Joins two strings
+        String s2 = str.concat(" Development");
+        System.out.println("Original String : " + str);
+        System.out.println("Concatenated String : " + s2);
+
+        // isEmpty() -> Checks whether string is empty
+        boolean empty = s2.isEmpty();
+        System.out.println("Is Empty : " + empty);
+
+        // indexOf() -> Returns first occurrence index
+        int firstIndex = s2.indexOf('t');
+        System.out.println("First Index : " + firstIndex);
+
+        // lastIndexOf() -> Returns last occurrence index
+        int lastIndex = s2.lastIndexOf('t');
+        System.out.println("Last Index : " + lastIndex);
+
+        // equals() -> Compares two strings
+        boolean equal = s2.equals("Python Development");
+        System.out.println("Equals : " + equal);
+
+        // equalsIgnoreCase() -> Ignores uppercase/lowercase
+        boolean ignoreCase = s2.equalsIgnoreCase("python development");
+        System.out.println("Equals Ignore Case : " + ignoreCase);
+
+        // charAt() -> Returns character at given index
+        char ch = s2.charAt(3);
+        System.out.println("Character : " + ch);
+
+        // toCharArray() -> Converts String into character array
+        char[] chArray = s2.toCharArray();
+
+        for (int i = 0; i < chArray.length; i++) {
+            System.out.println(chArray[i]);
+        }
+
+       
+    }
+}
