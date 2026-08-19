d2a5216cf5104eceb6d0e9466b6fe70c13a6e983
yogi2021 <yp628256@gmail.com>
2026-08-19 12:23:21 +0530

    Learning string

---
+++ Strings/String1.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class String1 {
+  public static void main(String[] args) {
+	 String s = new String("Java");
+	 System.out.println(s);
+	 
+	 String s2 = "Python";
+	 System.out.println(s2);
+	 
+	 char ch[]= {'N', 'O', 'P', 'D'};
+	 System.out.println(ch);
+}
+}
