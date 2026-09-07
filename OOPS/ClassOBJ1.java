6891c82b49fce696d8dc2f92ecf9a88d1acdf9cb
yogi2021 <yp628256@gmail.com>
2026-09-07 14:49:06 +0530

    Java OOPS: Class and Object..

--- OOPS/ClassOBJ1.java
+++ OOPS/ClassOBJ1.java
@@ -24,13 +24,6 @@ public class ClassOBJ1{
 	   
 	   Co1.display();
 	   
-	   ClassOBJ1 Co2= new ClassOBJ1 ();
-	   Co2.eid=102;
-	   Co2.ename="shreyash";
-	   Co2.job="Software Engineer";
-	   Co2.sal=100000;
-	   System.out.println();
-	   System.out.println(Co2.eid);
-	   System.out.println(Co2.ename);
+	   
 }
 }
