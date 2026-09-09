29ebc66a1ddf8ae192547c5d056ff50733a23db1
yogi2021 <yp628256@gmail.com>
2026-09-09 14:32:52 +0530

    OOPS: this keyword practice..

--- OOPS/ThisKeyword4.java
+++ OOPS/ThisKeyword4.java
@@ -8,8 +8,10 @@ public class ThisKeyword4 {
 	}
 	
 	void display() {
+        System.out.println("This Keyword: ");
 		System.out.println(a);
 		System.out.println(b);
+        System.out.println("------------------");
 	}
   public static void main(String[] args) {
 	  ThisKeyword4 th = new  ThisKeyword4(100, 500);
