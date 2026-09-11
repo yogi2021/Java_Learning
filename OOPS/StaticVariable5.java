2e966d1a9a2a67282887e05e2ecb84f06b84be77
yogi2021 <yp628256@gmail.com>
2026-09-11 15:04:58 +0530

    OOPS : Static Methods..

--- OOPS/StaticVariable5.java
+++ OOPS/StaticVariable5.java
@@ -1,27 +1,41 @@
 package OOPS;
 
 public class StaticVariable5 {
-  static int a=10;    //static variable
-  int b=20;           // non-static variable
-  
+    static int a = 10; // static variable
+    int b = 20; // non-static variable
 
-  
-  public static void main(String[] args) {
-    System.out.println("Statis variable: ");
-  System.out.println(a);
-  System.out.println("--------------");
-  System.out.println("Non Static variable: ");
- // m1();
-  
-  
-//  System.out.println(b);       //can not access directly without object
-//  System.out.println( m2());
-  StaticVariable5 sv = new StaticVariable5();
-  System.out.println(sv.b);                   //access using object
-  //sv.m2();
-	  
-  //sv.m();
-}
-  
-  
+    static void m1() { // static method
+        System.out.println("m1: This is static method");
+    }
+
+    void m2() { // non static method
+        System.out.println("m2: This is simple method");
+    }
+
+    void m() {
+        System.out.println(a);
+        System.out.println(b);
+        m1();
+        m2();
+    }
+
+    public static void main(String[] args) {
+
+        // System.out.println(b); //can not access directly without object
+        // System.out.println( m2());
+        StaticVariable5 sv = new StaticVariable5();
+        System.out.println(sv.b); // access using object
+        sv.m2();
+
+        
+
+        System.out.println("Statis variable: ");
+        System.out.println(a);
+        System.out.println("--------------");
+        //System.out.println("Non Static variable: ");
+        m1();
+
+        sv.m();
+    }
+
 }
